c2f69d1bc9437667b5c8b5a9eb7c9ebe436930c4
MeowGuaurr <Reynitamercedes@gmail.com>
2025-03-14 03:01:00 -0400

    set default port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const todosRoutes = require('./routes/todosRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Set the port from the environment variable or fallback to 5000
 
 app.use(cors());
 app.use(express.json()); // Parse JSON request bodies
@@ -17,11 +17,7 @@ app.get('/', (req, res) => {
   res.send('Todo API is running!');
 });
 
-// Start the server
+// Start the server using the defined port
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
